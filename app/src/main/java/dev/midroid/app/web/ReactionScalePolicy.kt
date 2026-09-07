6a5c8acc15ad9bfdf4498fd16b703d58da5a0cae
SMB-Chan <keimeisya03@gmail.com>
2026-09-07 12:33:55 +0900

    feat: scale note and notification reactions

--- app/src/main/java/dev/midroid/app/web/ReactionScalePolicy.kt
+++ app/src/main/java/dev/midroid/app/web/ReactionScalePolicy.kt
@@ -6,8 +6,10 @@ data class ReactionScaleMetrics(
     val buttonHeightCssPx: Int,
     val buttonFontPercent: Int,
     val horizontalPaddingCssPx: Int,
+    val noteEmojiCssPx: Int,
     val deckCellCssPx: Int,
     val deckEmojiCssPx: Int,
+    val notificationEmojiCssPx: Int,
 )
 
 object ReactionScalePolicy {
@@ -17,22 +19,28 @@ object ReactionScalePolicy {
                 buttonHeightCssPx = 52,
                 buttonFontPercent = 200,
                 horizontalPaddingCssPx = 8,
+                noteEmojiCssPx = 36,
                 deckCellCssPx = 48,
                 deckEmojiCssPx = 32,
+                notificationEmojiCssPx = 24,
             )
             ReactionScale.LARGE -> ReactionScaleMetrics(
                 buttonHeightCssPx = 60,
                 buttonFontPercent = 230,
                 horizontalPaddingCssPx = 10,
+                noteEmojiCssPx = 44,
                 deckCellCssPx = 56,
                 deckEmojiCssPx = 40,
+                notificationEmojiCssPx = 30,
             )
             ReactionScale.EXTRA_LARGE -> ReactionScaleMetrics(
                 buttonHeightCssPx = 68,
                 buttonFontPercent = 260,
                 horizontalPaddingCssPx = 12,
+                noteEmojiCssPx = 52,
                 deckCellCssPx = 64,
                 deckEmojiCssPx = 48,
+                notificationEmojiCssPx = 36,
             )
         }
     }
